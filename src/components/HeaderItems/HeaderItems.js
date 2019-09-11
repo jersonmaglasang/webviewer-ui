3740de03003d1a878f23b041ca6178e9523c9c51
achenPDFTron <achen@pdftron.com>
2019-09-11 08:15:06 -0700

    i got someothing...

--- src/components/HeaderItems/HeaderItems.js
+++ src/components/HeaderItems/HeaderItems.js
@@ -19,6 +19,7 @@ class HeaderItems extends React.PureComponent {
     const { items } = this.props;
     return (
       <div className="HeaderItems">
+        <button onClick={() => alert('clicked')} onTouchStart={() => alert('touched start')}>Click me</button>
         {items.map((item, i) => {
           const { type, dataElement, hidden } = item;
           const mediaQueryClassName = hidden ? hidden.map(screen => `hide-in-${screen}`).join(' ') : `${item.className || ''}`;
